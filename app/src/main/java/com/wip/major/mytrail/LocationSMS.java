988a5f576a35de47b86ba39b598bb7973ccb6a2a
Ram Tiwari <ramt24680@gmail.com>
2019-02-10 12:13:55 +0530

    tracking started

---
+++ app/src/main/java/com/wip/major/mytrail/LocationSMS.java
@@ -0,0 +1,29 @@
+package com.wip.major.mytrail;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
+import android.telephony.SmsManager;
+
+public class LocationSMS {
+    String phone;
+    String msg;
+
+    public LocationSMS(String phone, String msg){
+        this.phone = phone;
+        this.msg = msg;
+    }
+
+
+    protected void checkPermission(){
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED){
+
+        }
+    }
+
+    protected void sendSMS(){
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(phone, null, msg, null, null);
+    }
+}
