72d3beffc314ae28721d5a759f7c1fa2ba037252
Ram Tiwari <ramt24680@gmail.com>
2019-02-21 15:57:18 +0530

    signup done

--- app/src/main/java/com/wip/major/mytrail/Tracking_Window.java
+++ app/src/main/java/com/wip/major/mytrail/Tracking_Window.java
@@ -1,7 +1,14 @@
 package com.wip.major.mytrail;
 
+import android.Manifest;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -32,14 +39,13 @@ public class Tracking_Window extends FragmentActivity implements OnMapReadyCallb
         mTrack.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
+                if (isChecked) {
                     Intent intent = getIntent();
                     String email = intent.getStringExtra("email");
                     String pass = intent.getStringExtra("pass");
                     String uid = intent.getStringExtra("uid");
                     new StartTracking(getApplicationContext(), email, pass, uid);
-                }
-                else{
+                } else {
 
                 }
             }
@@ -60,8 +66,51 @@ public class Tracking_Window extends FragmentActivity implements OnMapReadyCallb
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        final LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Location location;
+                LocationListener listener = new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+
+                    }
+                };
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 600, 100, listener );
+                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if(location == null){
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    LatLng current = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(current).title("Current Location"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
+                }
+
+
+        }
+
     }
 }
